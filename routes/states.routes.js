ecdc6117efa8cb6ad3caeebe39ccfb6b973e169d
dwadasi siva teja <sivateja1034@gmail.com>
2023-06-23 18:55:26 +0530

    adding routes

--- routes/states.routes.js
+++ routes/states.routes.js
@@ -9,28 +9,27 @@ const states = require("../schemas/states");
 
 router.post("/states", async (req, res, next) => {
   try {
-    console.log(req.body);
     const state = new states(req.body);
     await state.save();
     res.status(201).json({ success: true, data: state });
   } catch (err) {
-    console.log(err)
     res.status(500).json({ error: err });
   }
 });
 
 /**
  * METHOD: get
- * fetching of the details. 
+ * fetching of the details.
  */
 router.get("/states", async (req, res, next) => {
   try {
-    let code = req.params.code || {};
+    let code = req.query.code
+      ? { code: String(req.query.code).toUpperCase() }
+      : {};
     const result = await states.find(code, { state: 1, code: 1 });
     res.status(200).json({ success: true, data: result });
   } catch (err) {
-    console.log(err)
-    res.status(500).json({error:err});
+    res.status(500).json({ error: err });
   }
 });
 
